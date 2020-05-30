74f0a6bbe9a6a9fa1320e3fb4415bcdfd61f054a
Han Bui <han.bui2894@gmail.com>
2020-05-30 11:57:21 -0500

    change site title

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <!-- Scale the content inside the device  -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/st.css" type="text/css">
-    <title>Index/Login Page</title>
+    <title>COVID-19 Consent Form</title>
 
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
